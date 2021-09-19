280785b5a399a7c51ab713820a8ba8ac53f9a284
Ikram Hasan <ikramhasan.dev@gmail.com>
2021-09-19 16:30:52 +0600

    Added Lecture 3

---
+++ Lecture 3/main.dart
@@ -0,0 +1,8 @@
+import 'student.dart';
+
+void main() {
+  Student student = Student();
+
+  student.show("Mahbub", 22);
+  student.show("Ikram", 23);
+}
---
+++ Lecture 3/student.dart
@@ -0,0 +1,5 @@
+class Student {
+  show(name, age) {
+    print("$name $age");
+  }
+}
