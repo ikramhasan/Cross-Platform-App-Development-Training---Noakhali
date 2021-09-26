e04a9dcb9dff001352e9bcafe7ebce386a3c2310
Ikram Hasan <ikramhasan.dev@gmail.com>
2021-09-26 12:15:45 +0600

    Added Lecture 6

---
+++ Lecture 6/user_input/lib/form_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class FormPage extends StatefulWidget {
+  FormPage({Key? key}) : super(key: key);
+
+  @override
+  State<FormPage> createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  TextEditingController firstNumberController = TextEditingController();
+
+  TextEditingController secondNumberController = TextEditingController();
+
+  int sum = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Input'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(34.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "$sum",
+                style: TextStyle(fontSize: 50),
+              ),
+              TextFormField(
+                controller: firstNumberController,
+                decoration: InputDecoration(
+                  labelText: "Enter a number",
+                  hintText: "eg. 23",
+                  border: OutlineInputBorder(),
+                ),
+                autovalidateMode: AutovalidateMode.always,
+                validator: (value) {
+                  RegExp regex = RegExp(r'\d$');
+                  if (!regex.hasMatch(value!)) {
+                    return 'Only numbers are allowed';
+                  }
+                },
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: secondNumberController,
+                decoration: InputDecoration(
+                  labelText: "Enter another number",
+                  hintText: "eg. 30",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  int num1 = int.parse(firstNumberController.text);
+                  int num2 = int.parse(secondNumberController.text);
+                  setState(() {
+                    sum = num1 + num2;
+                  });
+                },
+                child: Text("Add"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
